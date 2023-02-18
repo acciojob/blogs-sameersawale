b4081b926a30a77dd533c02533d4f6eed5a0d3f4
sameersawale <60250589+sameersawale@users.noreply.github.com>
2023-02-18 18:36:37 +0530

    solution

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -23,11 +23,28 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        User user=userRepository1.findById(userId).get();
+
+        Blog blog=new Blog();
+
+        blog.setTitle(title);
+        blog.setContent(content);
+
+        List<Blog> blogList=user.getBlogList();
+        blogList.add(blog);
+
+        blog.setUser(user);
+
+        user.setBlogList(blogList);
+        userRepository1.save(user);
+
+        return blog;
 
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        blogRepository1.deleteById(blogId);
 
     }
 }
